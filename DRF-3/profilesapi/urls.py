380fc628cc6cf0100c9dbd64efa62e248e894e78
jhell85 <jhell85@gmail.com>
2022-02-01 18:28:56 -0800

    add routes for api api-auth rest-auth

--- DRF-3/profilesapi/urls.py
+++ DRF-3/profilesapi/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include("profiles.api.urls")),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/rest-auth/", include("rest_auth.urls")),
+    path("api/rest-auth/registration/", include("rest_auth.registration.urls")),
+
+
 ]
 
 
