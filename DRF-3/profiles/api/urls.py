c005dfdc5732efbd829167f07b6f9fc5da287a08
jhell85 <jhell85@gmail.com>
2022-02-03 14:58:09 -0800

    use DRF router for routes of profiles and status made route to change user avatar

--- DRF-3/profiles/api/urls.py
+++ DRF-3/profiles/api/urls.py
@@ -1,7 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from profiles.api.views import ProfileViewSet, ProfileStatusViewSet, AvatarUpdateView
 
-from profiles.api.views import ProfileList
+
+# """
+#     This is for DRF.viewsets pattern 
+#     https://www.django-rest-framework.org/api-guide/viewsets/
+# """
+# profile_list = ProfileViewSet.as_view({"get": "list"})
+# profile_detail = ProfileViewSet.as_view({"get": "retrieve"})
+
+# urlpatterns = [
+#     path("profiles/", profile_list, name="profile-list"),
+#     path("profiles/<int:pk>/", profile_detail, name="profile-detail")
+# ]
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"profiles", ProfileViewSet)
+router.register(r"status", ProfileStatusViewSet)
 
 urlpatterns = [
-    path("profiles/", ProfileList.as_view(), name="profile-list")
+    path("", include(router.urls)),
+    path("avatar/", AvatarUpdateView.as_view(), name="avatar-update")
 ]
