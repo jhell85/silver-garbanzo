c33d8a6f1a23e9953a0a8973fd3b0aba43bfb717
jhell85 <jhell85@gmail.com>
2022-02-04 13:30:06 -0800

    test case for registration

--- DRF-3/profiles/tests.py
+++ DRF-3/profiles/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import imp
+import json
+from urllib import response
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from profiles.api.serializers import ProfileSerializer
+from profiles.models import Profile
+
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {"username": "testCase", "email": "test@test.com", 
+                "password1": "My_Strong-Password", "password2": "My_Strong-Password"}
+        response = self.client.post("/api/rest-auth/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
